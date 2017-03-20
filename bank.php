98520f0e099e8fbabc6170f7c5ec0ba3ce0eb80c
Tony <tony71318@gmail.com>
2017-03-20 12:18:56 +0800

    modualize

--- bank.php
+++ bank.php
@@ -394,82 +394,11 @@
 	<!-- 最近交易結束 -->
 
 	<!-- 合作銀行列表開始 -->
-	<div id="coop" class="container">
-		<div class="row">
-			<div class="col-xs-12">
-				<h3 class="text-center"><b>合作銀行</b></h3>
-				<hr />
-			</div>
-		</div>
-		<div id="coop-bank" class="row">
-			<div class="col-xs-6 col-sm-2">
-				<div class="thumbnail text-center">
-					<img alt="" src="images/yuanta_logo.png" />
-					<div class="caption">
-						<h3 class="h4">元大銀行</h3>						
-						<p><a href="https://ebank.yuantabank.com.tw/ib/" class="btn btn-primary btn-lg" role="button">進入網站</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-2">
-				<div class="thumbnail text-center">
-					<img alt="" src="images/cathay_logo.png"/>
-					<div class="caption">
-						<h3 class="h4">國泰世華銀行</h3>						
-						<p><a href="https://www.mybank.com.tw/mybank" class="btn btn-primary btn-lg" role="button">進入網站</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-2">
-				<div class="thumbnail text-center">
-					<img alt="" src="images/citi_logo.png" />
-					<div class="caption">
-						<h3 class="h4">花旗銀行</h3>						
-						<p><a href="https://www.citibank.com.tw/" class="btn btn-primary btn-lg" role="button">進入網站</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-2">
-				<div class="thumbnail text-center">
-					<img alt="" src="images/yuanta_logo.png" />
-					<div class="caption">
-						<h3 class="h4">元大銀行</h3>						
-						<p><a href="https://ebank.yuantabank.com.tw/ib/" class="btn btn-primary btn-lg" role="button">進入網站</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-2">
-				<div class="thumbnail text-center">
-					<img alt="" src="images/cathay_logo.png"/>
-					<div class="caption">
-						<h3 class="h4">國泰世華銀行</h3>						
-						<p><a href="https://www.mybank.com.tw/mybank" class="btn btn-primary btn-lg" role="button">進入網站</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-6 col-sm-2">
-				<div class="thumbnail text-center">
-					<img alt="" src="images/citi_logo.png" />
-					<div class="caption">
-						<h3 class="h4">花旗銀行</h3>						
-						<p><a href="https://www.citibank.com.tw/" class="btn btn-primary btn-lg" role="button">進入網站</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+		<?php include('php/part/cooperate_banklist.php'); ?>
 	<!-- 合作銀行列表結束 -->
 
 	<!-- 頁尾  -->
-	<footer id="footer" class="container-fluid text-center">
-		<hr/>
-		<a href="#myPage" title="To Top">
-			<span class="glyphicon glyphicon-chevron-up"></span>
-			<div>
-				<label for="To Top">To Top</label>
-			</div>
-		</a>
-	</footer>	
+		<?php include('php/part/footer.php'); ?>	
 	<!-- 頁尾結束  -->
 
 </body>
