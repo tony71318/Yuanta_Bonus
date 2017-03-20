63b47a12b188b9766b01145c91b3e26f29be71ea
Tony <tony71318@gmail.com>
2017-03-20 13:11:55 +0800

    clean useless files

--- php/Part/footer.php
+++
@@ -1,9 +0,0 @@
-<footer id="footer" class="container-fluid text-center">
-		<hr/>
-		<a href="#myPage" title="To Top">
-			<span class="glyphicon glyphicon-chevron-up"></span>
-			<div>
-				<label for="To Top">To Top</label>
-			</div>
-		</a>
-	</footer>
